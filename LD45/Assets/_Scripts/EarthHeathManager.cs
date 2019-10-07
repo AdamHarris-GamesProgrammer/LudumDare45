e334d6d1376c1c06c9988e21751d58c9f0df9f3a
Proagjin <gandalfthebignose@gmail.com>
2019-10-07 20:47:45 +0100

    add stuff

---
+++ LD45/Assets/_Scripts/EarthHeathManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EarthHeathManager : MonoBehaviour
+{
+    //global variable of how many building bought
+    //each building placed should +1 a integer
+    public int tempCounter;
+
+    public GameObject EarthSprite;
+    public GameObject EarthFaceSprite;
+
+    public Sprite Earth1;
+    public Sprite Earth1Face;
+    public Sprite Earth2;
+    public Sprite Earth2Face;
+    public Sprite Earth3;
+    public Sprite Earth3Face;
+    public Sprite Earth4;
+    public Sprite Earth4Face;
+    public Sprite Earth5;
+    public Sprite Earth5Face;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if building count is less then 5 and song has not been played before then set to true and play
+        
+        if (tempCounter >= 5 && tempCounter < 8)
+        {
+            EarthSprite.gameObject.GetComponent<SpriteRenderer>().sprite = Earth1;
+            EarthFaceSprite.gameObject.GetComponent<SpriteRenderer>().sprite = Earth1Face;
+        }
+        if (tempCounter >= 12 && tempCounter < 14)
+        {
+            EarthSprite.gameObject.GetComponent<SpriteRenderer>().sprite = Earth2;
+            EarthFaceSprite.gameObject.GetComponent<SpriteRenderer>().sprite = Earth2Face;
+        }
+        if (tempCounter >= 14 && tempCounter < 16)
+        {
+            EarthSprite.gameObject.GetComponent<SpriteRenderer>().sprite = Earth3;
+            EarthFaceSprite.gameObject.GetComponent<SpriteRenderer>().sprite = Earth3Face;
+        }
+        if (tempCounter >= 16 && tempCounter < 18)
+        {
+            EarthSprite.gameObject.GetComponent<SpriteRenderer>().sprite = Earth4;
+            EarthFaceSprite.gameObject.GetComponent<SpriteRenderer>().sprite = Earth4Face;
+        }
+        if (tempCounter == 18)
+        {
+            EarthSprite.gameObject.GetComponent<SpriteRenderer>().sprite = Earth5;
+            EarthFaceSprite.gameObject.GetComponent<SpriteRenderer>().sprite = Earth5Face;
+        }
+    }
+}
---
+++ LD45/Assets/_Scripts/GameMusicManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusicManager : MonoBehaviour
+{
+
+    public AudioSource HappyMusic1;
+    public AudioSource HappyMusic2;
+    public AudioSource MehMusic;
+    public AudioSource SadMusic1;
+    public AudioSource SadMusic2;
+
+    bool song1played = false;
+    bool song2played = false;
+    bool song3played = false;
+    bool song4played = false;
+    bool song5played = false;
+
+    //each building placed should +1 a integer
+    public int tempCounter;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if building count is less then 5 and song has not been played before then set to true and play
+        if (tempCounter < 5 && song1played == false) {
+            song1played = true;
+            HappyMusic1.Play();
+        }
+        if (tempCounter >= 5 && tempCounter < 10 && song2played == false)
+        {
+            song2played = true;
+            HappyMusic1.Stop();
+            HappyMusic2.Play();
+        }
+        if (tempCounter >= 10 && tempCounter < 14 && song3played == false)
+        {
+            song3played = true;
+            HappyMusic2.Stop();
+            MehMusic.Play();
+        }
+        if (tempCounter >= 14 && tempCounter < 18 && song4played == false)
+        {
+            song4played = true;
+            MehMusic.Stop();
+            SadMusic1.Play();
+        }
+        if (tempCounter == 18 && song5played == false)
+        {
+            song5played = true;
+            SadMusic1.Stop();
+            SadMusic2.Play();
+        }
+    }
+}
